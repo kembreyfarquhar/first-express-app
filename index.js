178c0e5242f0a8af2b39399ee74dce5a2f4f6ea5
kembreyfarquhar <kembreyfarquhar@gmail.com>
2021-12-01 19:51:52 -0600

    adds dotenv and 2 extra routes

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
+require("dotenv").config();
+
+const PORT = process.env.PORT;
 
 const app = express();
 
 app.get("/", (req, res) => {
-  console.log("Inside get request");
-  res.send("CONTENT");
+  console.log('Inside default "/"');
+  res.send("DEFAULT");
 });
 
-app.listen(3333);
+app.get("/two", (req, res) => {
+  console.log('Inside "/two"');
+  res.send("PAGE TWO");
+});
+
+app.get("/research/:animal", (req, res) => {
+  const animal = req.params.animal;
+  res.send(`Researching ${animal}`);
+});
+
+app.listen(PORT);
